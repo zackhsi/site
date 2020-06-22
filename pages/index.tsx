34ea54529c526dc0b331f0b420369daa1006e711
Zack Hsi <zackhsi@gmail.com>
2020-06-22 20:46:37 +0800

    Create avatar for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,16 @@
 import Layout from "../components/Layout";
+import Avatar from "../components/Avatar";
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi, I'm Zack</h1>
+    <Avatar src="/avatar.jpg" alt="Zack Hsi's avatar" />
+    <p>
+      Hi, I'm Zack. I live in Singapore, where I build out Stripe's global
+      payments network.
+    </p>
+    <p>
+      My email is zackhsi@gmail.com.
+    </p>
   </Layout>
 );
 
